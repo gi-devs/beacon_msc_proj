a753483ae9379d4b916551e4aa724f92254ff6cc
Geraldine Ho <2466602h@student.gla.ac.uk>
2025-07-27 17:48:12 +0100

    mobile: setup test

---
+++ apps/mobile/__tests__/example.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+import Example from '@/components/Example';
+
+describe('<Example/ >', () => {
+  it('renders the welcome text', () => {
+    const { getByText } = render(<Example />);
+    expect(getByText('Welcome To Beacon')).toBeTruthy();
+  });
+});
---
+++ apps/mobile/components/Example.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Example = () => {
+  return (
+    <View>
+      <Text>Welcome To Beacon</Text>
+    </View>
+  );
+};
+
+export default Example;
